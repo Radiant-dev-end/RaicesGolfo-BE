5a66847d0571fdea46f4271da7c8b0a53db74d4b
danielafervenefwdcostarica-droid <danielafervenefwdcostarica@gmail.com>
2026-05-12 16:05:13 -0600

    app

--- BE/src/app.js
+++ BE/src/app.js
@@ -6,7 +6,33 @@ const sequelize = require("./config/db") // Llama al ORM y a la configuración d
 
 require("./index") // Llama a todos los modelos para que se creen en la base de datos, por medio de sequelize
 
+// SWAGGER
+const swaggerUi = require("swagger-ui-express")
+const swaggerJsdoc = require("swagger-jsdoc")
+
 app.use(express.json()) // El servidor va a entender JSON
+
+// CONFIG SWAGGER
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Raices del Golfo API",
+            version: "1.0.0",
+            description: "Documentacion inicial de la API"
+        },
+        servers: [
+            {
+                url: "http://localhost:3000"
+            }
+        ]
+    },
+    apis: ["./routes/*.js"]
+}
+
+const specs = swaggerJsdoc(options)
+
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(specs))
 
 /*
     Conexión por medio de  sequelize, con la base de datos, y luego levanta el servidor
