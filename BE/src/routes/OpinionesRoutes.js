b1e625872a367c446fde0a9e78afc85d7679e939
Radiant-dev-end <ahernandezfwdcostarica@gmail.com>
2026-05-14 13:29:42 -0600

    RESERV

--- BE/src/routes/OpinionesRoutes.js
+++ BE/src/routes/OpinionesRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-<<<<<<< HEAD
 const OpinionController = require("../controllers/OpinionesController");
 
 // Obtener todas las opiniones
@@ -18,23 +17,5 @@ router.put("/:id", OpinionController.update);
 
 // Eliminar opinión
 router.delete("/:id", OpinionController.delete);
-=======
-const OpinionesController = require("../controllers/OpinionesController");
-
-// Obtener todas las opiniones
-router.get("/obtener", OpinionesController.getAll);
-
-// Obtener opinión por ID
-router.get("/obtener/:id", OpinionesController.getById);
-
-// Crear opinión
-router.post("/crear", OpinionesController.create);
-
-// Actualizar opinión
-router.put("/editar/:id", OpinionesController.update);
-
-// Eliminar opinión
-router.delete("/eliminar/:id", OpinionesController.delete);
->>>>>>> 4d6eba999c595b0e358a342207d677b4d5d30932
 
 module.exports = router;
