9d1abce269f2a926cb6bc6d8311e0d2cf3fe9109
Radiant-dev-end <ahernandezfwdcostarica@gmail.com>
2026-05-21 09:15:19 -0600

    Arreglos

--- BE/src/config/config.js
+++ BE/src/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     username: 'root',
-    password: 'root',
+    password: 'Ro.ot.2',
     database: 'raicesgolfo',
     host: 'localhost',
     dialect: 'mysql',
